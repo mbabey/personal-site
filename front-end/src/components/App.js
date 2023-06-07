af00e443949525c8b70ec5ced7fa9b3fb2e3ded2
Maxwell Babey <babeymax@gmail.com>
2023-06-07 14:15:43 -0700

    Dynamically change the size of the grid based on the number of elements

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,17 +1,22 @@
 import Grid from '../scripts/Grid'
 import React, { useState } from 'react';
+import '../styles/grid.css'
 
-const SIZE_X = 10;
-const SIZE_Y = 10;
 
 function App() {
-
+  
+  const SIZE_X = 10;
+  const SIZE_Y = 10;
+  const GRID_CONTAINER_SIZE = {gridTemplateColumns: `repeat(${SIZE_X}, 1fr)`, gridTemplateRows: `repeat(${SIZE_Y}, 1fr)`};
+  
   const [world, setWorld] = useState(new Grid(SIZE_X, SIZE_Y));
 
   return (
-    <>
+    <div 
+    className='grid-container' 
+    style={GRID_CONTAINER_SIZE}>
       {world.draw()}
-    </>
+    </div>
   );
 }
 
