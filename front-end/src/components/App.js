643f023e50b5f975988e8348c214745b49b7b2b7
Maxwell Babey <babeymax@gmail.com>
2023-09-08 11:27:13 -0400

    Decouple dijkstra pathfind from entity.

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,5 +1,6 @@
 import Grid from '../scripts/Grid'
 import Entity from '../scripts/Entity';
+import PriorityQueue from '../scripts/PriorityQueue';
 import React, { useState, useEffect } from 'react';
 import '../styles/grid.css'
 
@@ -22,7 +23,7 @@ function App() {
 
   useEffect(() => {
     async function run() {
-      const path_info = entity.dijkstra_pathfind();
+      const path_info = dijkstra_pathfind(entity, entity.get_target(), world);
 
       entity.set_path(path_info.s_path);
 
@@ -115,6 +116,65 @@ function get_target_start_location(size_x, size_y, entity_start_location) {
   const edge_cell_vertical = (size_y - 1) - entity_vertical;
 
   return { edge_cell_horizontal, edge_cell_vertical };
+}
+
+function dijkstra_pathfind(entity, target, world) {
+  const distances = {};
+  const path = {};
+  const priorityQueue = new PriorityQueue();
+  const initial_cell = entity.get_location();
+
+  // Set up the set of unvisited Cells. Set the distance of the currently populated Cell to 0.
+  for (let i = 0, row = 0; i < world.get_size(); ++i) {
+    if (i !== 0 && i % world.get_size_x() === 0) {
+      ++row;
+    }
+
+    const current_cell = world.get_cell(i % world.get_size_x(), row);
+
+    distances[current_cell.id] = Infinity;
+    path[current_cell.id] = null;
+  }
+  distances[initial_cell.id] = 0;
+
+  // Initialize the pQ with the initial cell and the initial cell distance (0).
+  priorityQueue.enqueue(initial_cell, distances[initial_cell.id]);
+
+  // Main loop.
+  while (!priorityQueue.isEmpty()) {
+    // Get the nearest cell and its neighbours.
+    const current_cell = priorityQueue.dequeue();
+    const neighbours = world.get_neighbours(current_cell);
+
+    for (let n in neighbours) {
+      const neighbour = neighbours[n];
+      // Get the abs. val. of the difference in altitude between a neighbour and the current cell.
+      const weight = Math.abs(current_cell.get_altitude() - neighbour.get_altitude());
+
+      // Total distance is the distance from the current cell to the initial cell + the change in alt.
+      const totalDistance = distances[current_cell.id] + weight;
+      // If the total distance is less than the known distance, a shorter path has been found.
+      if (totalDistance < distances[neighbour.id]) {
+        // Update the known distance and the path.
+        distances[neighbour.id] = totalDistance;
+        path[neighbour.id] = current_cell;
+
+        // Add the neighbour to the queue with the new distance as priority.
+        priorityQueue.enqueue(neighbour, totalDistance);
+      }
+    }
+  }
+
+  // The shortest path is the stored path from the target to the intial cell reversed.
+  const s_path = [];
+  for (let curr = target; curr !== null; curr = path[curr.id]) {
+    s_path.unshift(curr);
+  }
+
+  return {
+    s_path,
+    length: distances[target.id]
+  };
 }
 
 function async_wrapper(callback) {
--- front-end/src/scripts/Entity.js
+++ front-end/src/scripts/Entity.js
@@ -1,6 +1,4 @@
 
-import PriorityQueue from './PriorityQueue';
-
 /**
  * Entity.
  * The Entity moves across the Grid from Cell to Cell in the most efficient way.
@@ -55,9 +53,16 @@ class Entity {
    * Set the path for the Entity to follow.
    * @param {Cell[]} path the path to follow.
    */
-  set_path(path)
-  {
+  set_path(path) {
     this.#path = path;
+  }
+
+  /**
+   * Get this Entity's target Cell.
+   * @returns the target Cell.
+   */
+  get_target() {
+    return this.#target;
   }
 
   /**
@@ -73,72 +78,6 @@ class Entity {
     this.#location.toggle_populated();
   }
 
-  dijkstra_pathfind() {
-    const distances = {};
-    const path = {};
-    const priorityQueue = new PriorityQueue();
-    let initial_cell;
-
-    // Set up the set of unvisited Cells. Set the distance of the currently populated Cell to 0.
-    for (let i = 0, row = 0; i < this.#world.get_size(); ++i) {
-      if (i !== 0 && i % this.#world.get_size_x() === 0) {
-        ++row;
-      }
-
-      let current_cell = this.#world.get_cell(i % this.#world.get_size_x(), row);
-      let distance;
-
-      if (current_cell.get_populated()) {
-        initial_cell = current_cell;
-        distance = 0;
-      } else {
-        distance = Infinity;
-      }
-
-      distances[current_cell.id] = distance;
-      path[current_cell.id] = null;
-    }
-
-    // Initialize the pQ with the initial cell and the initial cell distance (0).
-    priorityQueue.enqueue(initial_cell, distances[initial_cell.id]);
-
-    // Main loop.
-    while (!priorityQueue.isEmpty()) {
-      // Get the nearest cell and its neighbours.
-      const current_cell = priorityQueue.dequeue();
-      const neighbours = this.#world.get_neighbours(current_cell);
-
-      for (let n in neighbours) {
-        const neighbour = neighbours[n];
-        // Get the abs. val. of the difference in altitude between a neighbour and the current cell.
-        const weight = Math.abs(current_cell.get_altitude() - neighbour.get_altitude());
-
-        // Total distance is the distance from the current cell to the initial cell + the change in alt.
-        const totalDistance = distances[current_cell.id] + weight;
-        // If the total distance is less than the known distance, a shorter path has been found.
-        if (totalDistance < distances[neighbour.id]) {
-          // Update the known distance and the path.
-          distances[neighbour.id] = totalDistance;
-          path[neighbour.id] = current_cell;
-
-          // Add the neighbour to the queue with the new distance as priority.
-          priorityQueue.enqueue(neighbour, totalDistance);
-        }
-      }
-    }
-
-    // The shortest path is the stored path from the target to the intial cell reversed.
-    const s_path = [];
-    for (let curr = this.#target; curr !== null; curr = path[curr.id]) {
-      s_path.unshift(curr);
-    }
-
-    return {
-      s_path,
-      length: distances[this.#target.id]
-    };
-  }
-  
 }
 
 export default Entity;
