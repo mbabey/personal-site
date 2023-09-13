5917223dce1ef8acdac2ffa4060872d8641b7d85
Maxwell Babey <babeymax@gmail.com>
2023-09-13 18:16:44 -0400

    Update styles for form. Add action and method to form.

--- front-end/src/components/SectionContact.js
+++ front-end/src/components/SectionContact.js
@@ -3,12 +3,15 @@ import React, { forwardRef } from 'react';
 import '../styles/contact.css';
 
 const Contact = forwardRef(function Contact(props, ref) {
+
+
+
   return (
     <section id='contact' ref={ref}>
-      <p>Contact me by email at babeymax@gmail.com or send me a note using the form below:</p>
-      <form>
-        <input type='text' placeholder='Name' required></input>
-        <input type='email' placeholder='Email' required></input>
+      <h2>Contact Me</h2>
+      <form autoComplete='on' action='/go' method='post'>
+        <input type='text' placeholder='Your Name' required></input>
+        <input type='email' placeholder='Your Email' required></input>
         <textarea type='text' placeholder='Message' required></textarea>
         <button type='submit'>Send</button>
       </form>
