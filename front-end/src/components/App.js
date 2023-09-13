2d6582a723c7900a630281d0bc27d66cc9f77046
Maxwell Babey <babeymax@gmail.com>
2023-09-13 11:06:12 -0400

    Add some prelim styles to about page

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import Pathfind from './Pathfind';
 import Nav from './Nav';
 import About from './SectionAbout';
--- front-end/src/components/SectionAbout.js
+++ front-end/src/components/SectionAbout.js
@@ -1,56 +1,62 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef } from 'react';
+
+import '../styles/about.css';
 
 const About = forwardRef(function About(props, ref) {
   return (
     <section id='about' ref={ref}>
       <h2>About</h2>
-      <div id='interests'>
-        <h3>Interests</h3>
-        <ul>
-          <li>Low-level Systems</li>
-          <li>Networking</li>
-          <li>Cybersecurity</li>
-          <li>Outdoorsmanship</li>
-        </ul>
-      </div>
-      <div id='proficiencies'>
-        <h3>Proficiencies</h3>
-        <div id='languages'>
-          <h4>Languages</h4>
+      <div className='wrapper'>
+        <div id='interests'>
+          <h3>Interests</h3>
           <ul>
-            <li>C</li>
-            <li>C++</li>
-            <li>Java</li>
-            <li>JavaScript</li>
-            <li>SQL</li>
+            <li>Low-level Systems</li>
+            <li>Networking</li>
+            <li>Cybersecurity</li>
+            <li>Outdoorsmanship</li>
           </ul>
         </div>
-        <div id='tools'>
-          <h4>Tools</h4>
+        <div id='proficiencies'>
+          <h3>Proficiencies</h3>
+          <div className='wrapper'>
+            <div id='languages'>
+              <h4>Languages</h4>
+              <ul>
+                <li>C</li>
+                <li>C++</li>
+                <li>Java</li>
+                <li>JavaScript</li>
+                <li>SQL</li>
+              </ul>
+            </div>
+            <div id='tools'>
+              <h4>Tools</h4>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>node.js</li>
+                <li>Express.js</li>
+                <li>React</li>
+                <li>mongodb</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div id='experience'>
+          <h3>Experience</h3>
           <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>node.js</li>
-            <li>Express.js</li>
-            <li>React</li>
-            <li>mongodb</li>
+            <li>Leading teams, large and small</li>
+            <li>Agile development</li>
+            <li>Project management</li>
+            <li>Solution design and analysis</li>
+            <li>Full-stack web applications</li>
+            <li>RESTful APIs</li>
+            <li>TCP- and UDP-based network applications</li>
+            <li>Berkeley Sockets programming</li>
+            <li>Application-layer network protocols</li>
+            <li>Algorithms and data structures</li>
           </ul>
         </div>
-      </div>
-      <div id='experience'>
-        <h3>Experience</h3>
-        <ul>
-          <li>Leading teams, large and small</li>
-          <li>Agile development</li>
-          <li>Project management</li>
-          <li>Solution design and analysis</li>
-          <li>Full-stack web applications</li>
-          <li>RESTful APIs</li>
-          <li>TCP- and UDP-based network applications</li>
-          <li>Berkeley Sockets programming</li>
-          <li>Application-layer network protocols</li>
-          <li>Algorithms and data structures</li>
-        </ul>
       </div>
     </section>
   )
