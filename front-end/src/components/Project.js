319bd68da9f162553e824b142bb9e7e3621d352c
Maxwell Babey <babeymax@gmail.com>
2023-09-20 18:36:03 -0400

    Update layout for projects

--- front-end/src/components/Project.js
+++ front-end/src/components/Project.js
@@ -7,18 +7,26 @@ function Project({ title, demolink, srclink, description, languages, images, ima
     <div className='project'>
       <div className='content'>
         <h3>{title}</h3>
-        {
-          demolink !== "" ?
-            <div>Demo: <a target='_blank' rel='noreferrer' href={demolink} tabIndex={-1}>{demolink}</a></div> :
-            <></>
-        }
-        {
-          srclink !== "" ?
-            <div>Source: <a target='_blank' rel='noreferrer' href={srclink} tabIndex={-1}>{srclink}</a></div> :
-            <></>
-        }
-        <p>{description}</p>
-        <p>{languages}</p>
+        <div className='links'>
+          {
+            demolink !== "" ?
+              <div><a target='_blank' rel='noreferrer' href={demolink} tabIndex={-1}>Demo</a></div> :
+              <></>
+          }
+          {
+            srclink !== "" ?
+              <div><a target='_blank' rel='noreferrer' href={srclink} tabIndex={-1}>Source Code</a></div> :
+              <></>
+          }
+        </div>
+        <div className='languages'>
+          <h4>Languages:</h4>
+          <p>{languages}</p>
+        </div>
+        <div className='description'>
+          <h4>Description:</h4>
+          <p>{description}</p>
+        </div>
       </div>
       <div className='images'>
         {images.map((imgsrc, index) => {
