39e3077ae0bde3083a951c04933801022d0dbf24
Maxwell Babey <babeymax@gmail.com>
2023-09-20 17:47:10 -0400

    Update styles for images. Add alternating images.

--- front-end/src/components/Project.js
+++ front-end/src/components/Project.js
@@ -2,27 +2,29 @@ import React from 'react';
 
 import '../styles/projects.css';
 
-function Project({title, demolink, srclink, description, languages, images, imagealts}) {
+function Project({ title, demolink, srclink, description, languages, images, imagealts }) {
   return (
     <div className='project'>
-      <h3>{title}</h3>
-      {
-        demolink !== "" ? 
-        <div>Demo: <a target='_blank' rel='noreferrer' href={demolink} tabIndex={-1}>{demolink}</a></div> :
-        <></>
-      }
-      {
-        srclink !== "" ? 
-        <div>Source: <a target='_blank' rel='noreferrer' href={srclink} tabIndex={-1}>{srclink}</a></div> :
-        <></>
-      }
-      <p>{description}</p>
-      <p>{languages}</p>
+      <div className='content'>
+        <h3>{title}</h3>
+        {
+          demolink !== "" ?
+            <div>Demo: <a target='_blank' rel='noreferrer' href={demolink} tabIndex={-1}>{demolink}</a></div> :
+            <></>
+        }
+        {
+          srclink !== "" ?
+            <div>Source: <a target='_blank' rel='noreferrer' href={srclink} tabIndex={-1}>{srclink}</a></div> :
+            <></>
+        }
+        <p>{description}</p>
+        <p>{languages}</p>
+      </div>
       <div className='images'>
         {images.map((imgsrc, index) => {
           return (
             <div key={imgsrc} className='img-wrapper'>
-              <img  src={imgsrc} alt={imagealts[index]}/>
+              <img src={imgsrc} alt={imagealts[index]} />
             </div>
           );
         })}
