2a4ef249e16eeafcd1392cd94cdcaa2786b64bdb
Maxwell Babey <babeymax@gmail.com>
2023-09-22 14:51:15 -0400

    Update contact form. Add email API for sending emails from website to personal email account. Add necessary helper APIs.

--- front-end/src/components/SectionContact.js
+++ front-end/src/components/SectionContact.js
@@ -1,18 +1,40 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useRef } from 'react';
+import { handleEnterReturnKeypress } from '../scripts/util';
+import emailjs from '@emailjs/browser';
 
 import '../styles/contact.css';
 
 const Contact = forwardRef(function Contact(props, ref) {
+
+  const form = useRef(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.EMAIL_SERVICE, process.env.EMAIL_TEMPLATE, form.current, process.env.EMAIL_KEY)
+      .then((result) => {
+        console.log(result);
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <section id='contact' ref={ref}>
       <div className='width-wrapper'>
         <h2>Contact</h2>
         <p>Reach out to me through the form below with your name, email, and message:</p>
-        <form autoComplete='on' action='/go' method='post'>
-          <input type='text' placeholder='Your Name' required></input>
-          <input type='email' placeholder='Your Email' required></input>
-          <textarea type='text' placeholder='Message' required></textarea>
-          <button type='submit'>Send</button>
+        <form autoComplete='on' ref={form}>
+          <input type='name' placeholder='Your Name' name='name' required></input>
+          <input type='email' placeholder='Your Email' name='email' required></input>
+          <textarea type='text' placeholder='Message' name='message' required></textarea>
+          <button
+            type='submit'
+            onClick={handleSubmit}
+            onKeyDown={(e) => handleEnterReturnKeypress(e, () => handleSubmit(e))}
+          >
+            Send
+          </button>
         </form>
         <div className='socials'>
           <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/maxwell-babey'><img src='linkedin.svg' alt='LinkedIn' /></a>
