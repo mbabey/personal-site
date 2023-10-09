17d0add1c0da6183b407e8d71321c4a946917554
Maxwell Babey <babeymax@gmail.com>
2023-10-09 18:29:44 -0400

    Update styles for about section

--- front-end/src/components/SectionAbout.js
+++ front-end/src/components/SectionAbout.js
@@ -7,36 +7,25 @@ const About = forwardRef(function About(props, ref) {
     <section id='about' ref={ref}>
       <div className='width-wrapper'>
         <h2 className='title'>About</h2>
-        <div>
-          <h4 className='text-head'>Hello, welcome!</h4>
-          <p>I am a software developer with two years' experience. I have an intuitive understanding of computing and a strong interest in networking and cybersecurity. My strong work ethic and love of new experiences and challenges manifests itself in all aspects of my life. Thanks for visiting!</p>
-        </div>
-        <div className='ipe-wrapper'>
-          <div id='interests'>
-            <h3>Interests</h3>
-            <ul>
-              <li>Low-level Systems</li>
-              <li>Networking</li>
-              <li>Cybersecurity</li>
-              <li>Teaching</li>
-              <li>Outdoorsmanship</li>
-            </ul>
-          </div>
-          <div id='proficiencies'>
-            <h3>Proficiencies</h3>
-            <div className='lt-awrapper'>
-              <div id='languages'>
-                <h4>Languages</h4>
+        <p>I am a software developer with two years' experience and a specialization in computer networking.</p>
+        <div className='stats-wrapper'>
+          <div className='proficiencies'>
+            <h4>Proficiencies</h4>
+            <div className='lt-wrapper'>
+              <div className='languages'>
+                <h5>Languages</h5>
                 <ul>
                   <li>C</li>
                   <li>C++</li>
+                  <li>Go</li>
+                  <li>Python</li>
                   <li>Java</li>
                   <li>JavaScript</li>
                   <li>SQL</li>
                 </ul>
               </div>
-              <div id='tools'>
-                <h4>Tools</h4>
+              <div className='tools'>
+                <h5>Tools</h5>
                 <ul>
                   <li>HTML</li>
                   <li>CSS</li>
@@ -48,21 +37,31 @@ const About = forwardRef(function About(props, ref) {
               </div>
             </div>
           </div>
-          <div id='experience'>
-            <h3>Experience</h3>
-            <ul>
-              <li>Leading teams, large and small</li>
-              <li>Agile development</li>
-              <li>Project management</li>
-              <li>Solution design and analysis</li>
-              <li>Full-stack web applications</li>
-              <li>RESTful APIs</li>
-              <li>TCP- and UDP-based network applications</li>
-              <li>Berkeley Sockets programming</li>
-              <li>Application-layer network protocols</li>
-              <li>Algorithms and data structures</li>
-            </ul>
-          </div>
+            <div className='experience'>
+              <h4>Experience</h4>
+              <ul>
+                <li>Leading teams, large and small</li>
+                <li>Agile development</li>
+                <li>Project management</li>
+                <li>Solution design and analysis</li>
+                <li>Full-stack web applications</li>
+                <li>RESTful APIs</li>
+                <li>TCP- and UDP-based network applications</li>
+                <li>Berkeley Sockets programming</li>
+                <li>Application-layer network protocols</li>
+                <li>Algorithms and data structures</li>
+              </ul>
+            </div>
+            <div className='interests'>
+              <h4>Interests</h4>
+              <ul>
+                <li>Low-level Systems</li>
+                <li>Networking</li>
+                <li>Cybersecurity</li>
+                <li>Teaching</li>
+                <li>Outdoorsmanship</li>
+              </ul>
+            </div>
         </div>
       </div>
     </section>
