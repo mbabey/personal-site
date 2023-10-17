86e430153adef858ae73eee7f2394766d65456b0
Maxwell Babey <babeymax@gmail.com>
2023-10-17 15:47:55 -0400

    Update about section appearance

--- front-end/src/components/SectionAbout.js
+++ front-end/src/components/SectionAbout.js
@@ -4,40 +4,32 @@ import '../styles/about.css';
 
 const About = forwardRef(function About(props, ref) {
   return (
-    <section id='about' ref={ref}>
-      <div className='width-wrapper'>
+    <section id='about' className='width-wrapper' ref={ref}>
+      <div className='info-wrapper'>
         <h2 className='title'>About</h2>
-        <p className='bio'>Hello! I am a software developer with two years' experience, passion, and knowledge of everything below:</p>
-        <div className='wordcloud'>
-          <img src='wordcloud.svg' alt='wordcloud' />
-        </div>
+        <p className='bio'>Hello! I am a software developer with two years' experience and a passion for programming.</p>
         <div className='proficiencies'>
           <h4>Proficiencies</h4>
-          <div className='lt-wrapper'>
-            <div className='languages'>
-              <h5>Languages</h5>
-              <ul>
-                <li>C</li>
-                <li>C++</li>
-                <li>Go</li>
-                <li>Python</li>
-                <li>Java</li>
-                <li>JavaScript</li>
-                <li>SQL</li>
-              </ul>
-            </div>
-            <div className='tools'>
-              <h5>Tools</h5>
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>node.js</li>
-                <li>Express.js</li>
-                <li>React</li>
-                <li>mongodb</li>
-              </ul>
-            </div>
+          <div className='pillars'>
+            <div className='cn'>Computer Networking</div>
+            <div className='pm'>Project Management</div>
+            <div className='fs'>Full-stack</div>
           </div>
+          <ul>
+            <li>C</li>
+            <li>C++</li>
+            <li>Go</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>JavaScript</li>
+            <li>SQL</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>node.js</li>
+            <li>Express.js</li>
+            <li>React</li>
+            <li>mongodb</li>
+          </ul>
         </div>
       </div>
     </section>
