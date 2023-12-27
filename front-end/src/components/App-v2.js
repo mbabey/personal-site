5d6b900427cb0323806eece1be649362442a465e
Maxwell Babey <babeymax@gmail.com>
2023-12-27 16:44:17 -0500

    Add some preliminary styles for the pathfind update

--- front-end/src/components/App-v2.js
+++ front-end/src/components/App-v2.js
@@ -8,14 +8,19 @@ export default function App2() {
   const PATHFIND_SETTINGS = {
     size_x: 20,
     size_y: 20
-  }
+  };
 
-  const URL_GITHUB = 'https://github.com/mbabey';
-  const URL_LINKEDIN = 'https://www.linkedin.com/in/maxwell-babey';
+  const URLS = {
+    github: 'https://github.com/mbabey',
+    linkedin: 'https://www.linkedin.com/in/maxwell-babey'
+  };
 
   return (
     <>
-      <Pathfind size_x={PATHFIND_SETTINGS.size_x} size_y={PATHFIND_SETTINGS.size_y} />
+      {/* <Pathfind size_x={PATHFIND_SETTINGS.size_x} size_y={PATHFIND_SETTINGS.size_y} /> */}
+      <div className='pathfind-placeholder'>
+        Pathfind placeholder
+      </div>
       <div className='h-text'>
         <div className='name'>
           Maxwell Babey
@@ -28,16 +33,19 @@ export default function App2() {
         </div>
       </div>
       <div className='buttons'>
-        <button className='btn github' onClick={() => openUrl(URL_GITHUB)}>
+        <button className='btn github' onClick={() => openUrl(URLS.github)}>
           <img src='github.svg' alt='GitHub' />
         </button>
-        <button className='btn linkedin' onClick={() => openUrl(URL_LINKEDIN)}>
+        <button className='btn linkedin' onClick={() => openUrl(URLS.linkedin)}>
           <img src='linkedin.svg' alt='LinkedIn' />
         </button>
         <button className='btn contact'>
           Contact Me &gt;
         </button>
       </div>
+      {/* <div className='pathfind-bar'>
+        <div className=''></div>
+      </div> */}
     </>
   )
 }
