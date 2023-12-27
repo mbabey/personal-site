21ba9a58bb9dbe62da7bd2c09e34bd4d71f64b53
Maxwell Babey <babeymax@gmail.com>
2023-12-27 14:49:21 -0500

    Remove version 1 code. Begin version 2 code.

---
+++ front-end/src/components/App-v2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App2() {
+  return (
+    <>
+      <div>
+        <div className='name'>
+
+        </div>
+        <div className='purpose-state'></div>
+      </div>
+    </>
+  )
+}
