a0f57346cb2464c651a6b8968269b9ae8f2f3c6b
Maxwell Babey <babeymax@gmail.com>
2023-12-29 14:01:50 -0500

    Add a draw method to the cell. Call the Cell draw method from the Grid draw method.

--- front-end/src/pathfind/scripts/Cell.js
+++ front-end/src/pathfind/scripts/Cell.js
@@ -108,6 +108,36 @@ class Cell {
   get_visited() {
     return this.#visited;
   }
+
+  /**
+   * Draw the cell as a column with a specified size.
+   * @param {*} max_height_px the maximum height of the column.
+   * @param {*} min_height_px the minimum height of the column.
+   * @returns an Element with the properties and classes of the cell.
+   */
+  draw(max_height_px, min_height_px) {
+    const location = this.get_coordinates();
+    const altitude = this.get_altitude();
+    const populated = this.get_populated();
+    const targeted = this.get_targeted();
+    const visited = this.get_visited();
+
+    const cell_class = `cell${populated ? ' populated' : ''}${targeted ? ' targeted' : ''}${visited ? ' visited' : ''}`;
+    
+    const cell_height = (altitude / Cell.MAX_ALTITUDE) * max_height_px + min_height_px;
+    const cell_top = -cell_height;
+
+    const top_style = {top: `${cell_top}px`};
+    const tower_style = {height: `${cell_height}px`};
+
+    return (
+      <div key={[location.x, location.y]} className={`${cell_class} box ${location.x}-${location.y}`}>
+        <div className={`${cell_class} tower right`} style={tower_style}></div>
+        <div className={`${cell_class} tower left`} style={tower_style}></div>
+        <div className={`${cell_class} top`} style={top_style}></div>
+      </div>
+    );
+  }
 }
 
 export default Cell;
--- front-end/src/pathfind/scripts/Grid.js
+++ front-end/src/pathfind/scripts/Grid.js
@@ -101,33 +101,15 @@ class Grid {
   }
 
   /**
-   * For each Cell in the Grid, create an HTML element.
+   * Draw each Cell in the Grid.
+   * @param {*} max_height_px the maximum height of the column.
+   * @param {*} min_height_px the minimum height of the column.
    * @returns the array of HTML elements.
    */
   draw(max_height_px, min_height_px) {
     const image = this.#arr.map(row => {
       return row.map(cell => {
-        const location = cell.get_coordinates();
-        const altitude = cell.get_altitude();
-        const populated = cell.get_populated();
-        const targeted = cell.get_targeted();
-        const visited = cell.get_visited();
-
-        const cell_class = `cell${populated ? ' populated' : ''}${targeted ? ' targeted' : ''}${visited ? ' visited' : ''}`;
-        
-        const cell_height = (altitude / Cell.MAX_ALTITUDE) * max_height_px + min_height_px;
-        const cell_top = -cell_height;
-
-        const top_style = {top: `${cell_top}px`};
-        const tower_style = {height: `${cell_height}px`};
-
-        return (
-          <div key={[location.x, location.y]} className={`${cell_class} box`}>
-            <div className={`${cell_class} tower right`} style={tower_style}></div>
-            <div className={`${cell_class} tower left`} style={tower_style}></div>
-            <div className={`${cell_class} top`} style={top_style}></div>
-          </div>
-        );
+        return cell.draw(max_height_px, min_height_px);
       })
     });
 
